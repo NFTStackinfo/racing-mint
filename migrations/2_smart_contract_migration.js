97f858eaf5fa857635feb75b4f7a10b4d92df1fb
AshotSimonyan <a.simonyan.stdev@gmail.com>
2022-06-24 16:22:05 +0400

    test

--- migrations/2_smart_contract_migration.js
+++
@@ -1,5 +0,0 @@
-const SmartContract = artifacts.require("TheBlindClub");
-
-module.exports = function (deployer) {
-  deployer.deploy(SmartContract, 'http://94.237.98.208/?id=');
-};
--- src/data.js
+++
@@ -1,8 +0,0 @@
-const addressList = [
-    '0xbd869d89ba193d1590049ac3eff2ffb17c4c9b5b',
-    '0xe4a5510402e17ad8bed1d5f891f9c4d8f104e11c',
-    '0x9dbf14c79847d1566419dcddd5ad35daf0382e05',
-    '0xb831f40De050B094cffb5bC17e9746cAda70fA28'
-]
-
-export default addressList
